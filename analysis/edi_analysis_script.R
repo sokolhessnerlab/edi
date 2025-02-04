8f70925911b329e07cc4307d3ae8746877e982d2
Peter Sokol-Hessner <hessner@gmail.com>
2025-02-04 09:22:15 -0700

    update to handle age NAs
    Update means to handle age NAs
    
    Co-Authored-By: Sophie Forcier <155326965+sophieforcier@users.noreply.github.com>

--- analysis/edi_analysis_script.R
+++ analysis/edi_analysis_script.R
@@ -192,10 +192,10 @@ library(corrplot)
 # GRAPHS: histograms, scatterplots, correlograms, pairwise correlations
 
 # Age
-mean(clean_data_survey$age) # 19.8 years old (4/5/24)
-range(clean_data_survey$age) # 12-35 (4/5/24) I think there is a typo - cge074 put 12
-sd(clean_data_survey$age) # 2.63 years (4/5/24)
-hist(clean_data_survey$age)
+mean(clean_data_survey$age, na.rm = T) # 19.8 years old (4/5/24)
+range(clean_data_survey$age, na.rm = T) # 12-35 (4/5/24) I think there is a typo - cge074 put 12
+sd(clean_data_survey$age, na.rm = T) # 2.63 years (4/5/24)
+hist(clean_data_survey$age, na.rm = T)
 
 # Race (1 = American Indian/Alaska Native; 2 = Black/African-American; 3 = East Asian; 4 = Native Hawaiian/Pacific Islander; 5 = South Asian; 6 = White; 7 = Bi-/Multi-racial (text); 8 = Other (text); 9 = Prefer not to say)
 clean_data_survey$race # was never loaded for some reason
